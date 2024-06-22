19c514ca74c2e7f222a4695d603bac48bfd531bc
ryangreider <ryandaniel136@hotmail.com>
2024-06-22 03:58:44 -0500

    Hello

---
+++ db/migrate/20240622083844_create_items.rb
@@ -0,0 +1,13 @@
+class CreateItems < ActiveRecord::Migration[7.1]
+  def change
+    create_table :items do |t|
+      t.string :name
+      t.integer :quantity
+      t.string :category
+      t.decimal :cost
+      t.text :note
+
+      t.timestamps
+    end
+  end
+end
