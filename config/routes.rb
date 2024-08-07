31db211d2d73544ea0a94df5fc5108f9e7891e72
ryangreider <ryandaniel136@hotmail.com>
2024-08-06 20:50:42 -0500

    routes fix and verify database models, also home index to login

--- config/routes.rb
+++ config/routes.rb
@@ -2,14 +2,5 @@ Rails.application.routes.draw do
   #Items Model
   resources :items
   root 'items#index'
-  resources :items
-
-  #User Model
-  root 'sessions#home'
-  resources :users, only: [:new, :create, :update, :show, :destroy]
-
-  get '/login', 'sessions#login'
-  post '/login', 'sessions#create'
-  post '/logout', 'sessions#destroy'
-  get '/logout', 'sessions#destroy'
 end
+
