9b0f91e2350a8f175224a26a832286ba8a3df526
ryangreider <ryandaniel136@hotmail.com>
2024-08-15 15:59:27 -0500

    chi

--- config/routes.rb
+++ config/routes.rb
@@ -1,12 +1,7 @@
 Rails.application.routes.draw do
   #Items Model
   resources :items
-  root 'home#index'
-  resources :users, only: [:new, :create, :update, :show, :destroy]
-
- get '/login', 'sessions#login'
- post '/login', 'sessions#create'
- delete '/logout', 'sessions#destroy'
- get '/logout', 'sessions#destroy'
+  root 'items#index'
+  
 end
 
