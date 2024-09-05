d410ace4e9522f756bb772549fb03e6e775bcbb7
ryangreider <ryandaniel136@hotmail.com>
2024-09-05 17:28:16 -0500

    fixed login

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,3 +1,10 @@
 class ApplicationController < ActionController::Base
     before_action :authenticate_user!
+    
+    protected
+
+    def configure_permitted_parameters
+        devise_parameter_sanitizer.permit(:sign_up, keys: [:email, :password, :password_confirmation])
+        devise_parameter_sanitizer.permit(:account_update, keys: [:email, :password, :password_confirmation, :current_password])
+      end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,17 +1,10 @@
 Rails.application.routes.draw do
-  get 'sessions/new'
-  get 'sessions/create'
-  get 'sessions/destroy'
-  delete 'logout', to: 'sessions#destroy'
   devise_for :users
+
   resources :categories
   resources :suppliers
   resources :items
   resources :inventories
-  
-  get 'login', to: 'sessions#new'
-  post 'login', to: 'sessions#create'
-  delete 'logout', to: 'sessions#destroy'
 
   root 'items#index'
   
